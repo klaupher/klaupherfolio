4fff322c677dd535a6cd708a708ba23a58934420
Fernanda Bernardes <fernanda.bernardes@gsoftware.com.br>
2025-03-20 17:22:03 -0300

    feat(backend): schema de banco de dados concluído

---
+++ backend/prisma/migrations/20250320200805_tabelas_tecnologia_projeto/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "tecnologia" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "destaque" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "tecnologia_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "projeto" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "imagens" TEXT[],
+    "nivel" INTEGER NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "destaque" BOOLEAN NOT NULL DEFAULT false,
+    "repositorio" TEXT NOT NULL,
+
+    CONSTRAINT "projeto_pkey" PRIMARY KEY ("id")
+);
